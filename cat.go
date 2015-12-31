e46cae75e18376316a0a36eccb6d6fa3af4efe90
sanemat <o.gata.ken@gmail.com>
2015-12-31 18:18:35 +0900

    feat(cat): read from file

--- cat.go
+++ cat.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	// https://golang.org/pkg/bufio/#example_Scanner_lines
-	scanner := bufio.NewScanner(os.Stdin)
+	// http://qiita.com/ikawaha/items/28186d965780fab5533d
+	var fp *os.File
+	var err error
+
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		fp = os.Stdin
+	} else {
+		fp, err = os.Open(flag.Arg(0))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer fp.Close()
+	}
+	scanner := bufio.NewScanner(fp)
 	for scanner.Scan() {
 		fmt.Println(scanner.Text()) // Println will add back the final '\n'
 	}
