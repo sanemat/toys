aae9377a33ae04eff5a557b489d9e52f4bc0c88a
sanemat <o.gata.ken@gmail.com>
2015-12-31 14:37:11 +0900

    feat(cat): add implemantation

--- cat.go
+++ cat.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"os"
+	"fmt"
+	"bufio"
 )
 func main() {
-	os.Exit(1)
+	// https://golang.org/pkg/bufio/#example_Scanner_lines
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text()) // Println will add back the final '\n'
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
